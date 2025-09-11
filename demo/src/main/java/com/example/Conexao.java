ace4ff2c41d4e6777b9baad34568cacfa860ad44
rosamaduda <rosamaduda26@gmail.com>
2025-09-11 14:35:00 -0300

    mudando os nomes das variáveis do .env e fazendo pequenas alterações no Conexao.java

---
+++ demo/src/main/java/com/example/Conexao.java
@@ -0,0 +1,39 @@
+package com.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Conexao {
+    Dotenv dotenv = Dotenv.load(); // carregando as informações do .env
+
+    public Connection conectar(){
+        Connection conn = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            String usuario = dotenv.get("USUARIO");
+            String senha = dotenv.get("SENHA");
+            String url = dotenv.get("URL");
+            conn= DriverManager.getConnection(url,usuario,senha);
+            System.out.println("Conexão estabelecida com sucesso!");
+    }catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }catch (ClassNotFoundException f) {
+            System.out.println("Driver JDBC não encontrado: " + f.getMessage());
+        }finally{return conn;}
+    }
+
+
+
+    public void desconecar(Connection conn){
+        try{
+            if(conn!=null && !conn.isClosed()){
+                conn.close();
+            }
+        }catch (SQLException e) {
+            e.getMessage();
+        }
+    }
+    
+}
